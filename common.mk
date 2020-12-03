70b67bf3c161900b2b164e1ac899fa6713d3861e
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 11:44:40 +0530

    msm8998-common: power-libperfmgr: Adapt and rebrand for oneplus_msm8998
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- common.mk
+++ common.mk
@@ -366,7 +366,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.msm8998-libperfmgr
+    android.hardware.power-service.oneplus_msm8998-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
