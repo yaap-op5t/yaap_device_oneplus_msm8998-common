179e90dcf75557b3ee026d2d8fde99e0283ec171
enesykaya <enesykaya@gmail.com>
2021-02-06 02:04:32 +0300

    msm8998-common: Update fp to redfin February

--- init/init_oneplus_msm8998.cpp
+++ init/init_oneplus_msm8998.cpp
@@ -147,8 +147,8 @@ void init_fingerprint_properties()
 void vendor_load_properties()
 {
     // fingerprint
-    property_override("ro.build.description", "redfin-user 11 RQ1A.210105.003 7005429 release-keys");
-    property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ1A.210205.004 7038034 release-keys");
+    property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys");
     init_target_properties();
     init_fingerprint_properties();
     load_dalvikvm_properties();
