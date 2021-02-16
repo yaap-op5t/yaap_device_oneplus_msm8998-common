140c9d80e9d01801425d58298618006d996f0387
Georg Veichtlbauer <georg@vware.at>
2021-02-16 08:43:54 +0100

    msm8998-common: Remove perf related blobs and props
    Change-Id: Ibac1df023857008d210e16f659b47a55727d84f6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -176,7 +176,6 @@ TARGET_USES_MKE2FS := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
