LineageOS/android_device_oneplus_msm8998-common

msm8998-common: convert vendor_prop.mk to vendor.prop
 * Also move system properties to system.prop
 * Remove unused properties

Change-Id: I95dc1b5d0b7755a9367d7f40841a26fce6befaaa

BoardConfigCommon.mk
@@ -181,6 +181,7 @@ TARGET_USES_INTERACTION_BOOST := true
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab

yaap-op5t/yaap_device_oneplus_msm8998-common

BoardConfigCommon.mk
@@ -181,6 +181,7 @@ TARGET_USES_INTERACTION_BOOST := true
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
+TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab