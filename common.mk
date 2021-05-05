f987cadb901c378e28804766ce8080c406b6dbed
Georg Veichtlbauer <georg@vware.at>
2021-05-05 11:40:29 +0200

    msm8998-common: Remove errorneous Treble configuration
    * Property is useless
    * Packages are useless
    
    Partially reverts commit 38457a3dbcfec8c29f412a092e1bddd5463a08f8.
    
    Change-Id: I7d9b2d15b44ac01e3f690db69b137080faafd3bb

--- common.mk
+++ common.mk
@@ -173,8 +173,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system \
-    libvulkan
+    libqdMetaData.system
 
 PRODUCT_PACKAGES += \
     vendor.display.config@1.9 \
@@ -448,11 +447,6 @@ $(call inherit-product, build/target/product/verity.mk)
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # VNDK
-# Update this list with what each blob is actually for
-# libstdc++: camera.msm8998
-PRODUCT_PACKAGES += \
-    libstdc++.vendor
-
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
