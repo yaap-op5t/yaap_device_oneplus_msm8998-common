1a94898032386723e8cad42e74c53dbd77b22832
Ido Ben-Hur <idoybh2@gmail.com>
2021-08-24 20:06:45 +0300

    msm8998-common: DeviceSettings: Add sliders for color balance adjustment
    Not every device has the option in settings, this is the right place to add it
    
    Minor improvements to code, formatting and design while at it

--- DeviceSettings/src/com/yaap/device/DeviceSettings/PanelSettings.java
+++ DeviceSettings/src/com/yaap/device/DeviceSettings/PanelSettings.java
@@ -22,6 +22,7 @@ import android.app.Dialog;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.display.ColorDisplayManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -36,32 +37,42 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.util.Log;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.TwoStatePreference;
 import androidx.viewpager.widget.ViewPager;
 
-import com.android.internal.util.yaap.FileUtils;
 import com.yaap.device.DeviceSettings.ModeSwitch.*;
+import java.util.Locale;
 
-public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCheckedChangeListener {
-    private RadioGroup mRadioGroup;
+public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCheckedChangeListener,
+        SeekBar.OnSeekBarChangeListener {
+    private static final int COLOR_CHANNEL_RED = 0;
+    private static final int COLOR_CHANNEL_GREEN = 1;
+    private static final int COLOR_CHANNEL_BLUE = 2;
+
     ViewPager viewPager;
     LinearLayout sliderDotspanel;
     private int dotscount;
     private ImageView[] dots;
+    private ColorDisplayManager mColorDisplayManager;
+
+    private SeekBar mRedPref;
+    private SeekBar mGreenPref;
+    private SeekBar mBluePref;
+    private TextView mRedText;
+    private TextView mGreenText;
+    private TextView mBlueText;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mRadioGroup = (RadioGroup) view.findViewById(R.id.radio_group);
+        RadioGroup mRadioGroup = view.findViewById(R.id.radio_group);
         int checkedButtonId = R.id.off_mode;
         if (NightModeSwitch.isCurrentlyEnabled()) {
             checkedButtonId = R.id.night_mode;
@@ -76,37 +87,82 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
         }
         mRadioGroup.check(checkedButtonId);
         mRadioGroup.setOnCheckedChangeListener(this);
+
+        if (ColorDisplayManager.isColorTransformAccelerated(getContext())) {
+            mColorDisplayManager = getContext().getSystemService(ColorDisplayManager.class);
+            mRedPref = view.findViewById(R.id.color_balance_red);
+            mRedPref.setOnSeekBarChangeListener(this);
+            mGreenPref = view.findViewById(R.id.color_balance_green);
+            mGreenPref.setOnSeekBarChangeListener(this);
+            mBluePref = view.findViewById(R.id.color_balance_blue);
+            mBluePref.setOnSeekBarChangeListener(this);
+
+            mRedText = view.findViewById(R.id.color_balance_red_percent);
+            mGreenText = view.findViewById(R.id.color_balance_green_percent);
+            mBlueText = view.findViewById(R.id.color_balance_blue_percent);
+
+            mRedPref.setProgress(mColorDisplayManager.getColorBalanceChannel(COLOR_CHANNEL_RED));
+            mGreenPref.setProgress(mColorDisplayManager.getColorBalanceChannel(COLOR_CHANNEL_GREEN));
+            mBluePref.setProgress(mColorDisplayManager.getColorBalanceChannel(COLOR_CHANNEL_BLUE));
+        } else {
+            LinearLayout slidersCategory = view.findViewById(R.id.rgb_category);
+            slidersCategory.setVisibility(View.GONE);
+            slidersCategory.setEnabled(false);
+        }
     }
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        String percent = String.format(Locale.US, "%d%%",
+                Math.round((progress - 25) / 2.3));
+        if (seekBar == mRedPref) {
+            mColorDisplayManager.setColorBalanceChannel(COLOR_CHANNEL_RED, progress);
+            mRedText.setText(percent);
+        } else if (seekBar == mGreenPref) {
+            mColorDisplayManager.setColorBalanceChannel(COLOR_CHANNEL_GREEN, progress);
+            mGreenText.setText(percent);
+        } else if (seekBar == mBluePref) {
+            mColorDisplayManager.setColorBalanceChannel(COLOR_CHANNEL_BLUE, progress);
+            mBlueText.setText(percent);
+        }
     }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) { }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) { }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-	final View rootView = inflater.inflate(R.layout.panel_modes, container, false);
+        final View rootView = inflater.inflate(R.layout.panel_modes, container, false);
 
-	viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
-        sliderDotspanel = (LinearLayout) rootView.findViewById(R.id.SliderDots);
+        viewPager = rootView.findViewById(R.id.viewPager);
+        sliderDotspanel = rootView.findViewById(R.id.SliderDots);
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getActivity());
         viewPager.setAdapter(viewPagerAdapter);
 
         dotscount = viewPagerAdapter.getCount();
         dots = new ImageView[dotscount];
 
-        for(int i = 0; i < dotscount; i++){
+        for (int i = 0; i < dotscount; i++) {
             dots[i] = new ImageView(getActivity());
-            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.inactive_dot));
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),
+                    R.drawable.inactive_dot));
 
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             params.setMargins(8, 0, 8, 0);
             sliderDotspanel.addView(dots[i], params);
         }
-        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),
+                R.drawable.active_dot));
 
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
             }
@@ -115,16 +171,18 @@ public class PanelSettings extends PreferenceFragment implements RadioGroup.OnCh
             public void onPageSelected(int position) {
 
                 for(int i = 0; i< dotscount; i++){
-                    dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.inactive_dot));
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(
+                            getActivity().getApplicationContext(), R.drawable.inactive_dot));
                 }
-                dots[position].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.active_dot));
+                dots[position].setImageDrawable(ContextCompat.getDrawable(
+                        getActivity().getApplicationContext(), R.drawable.active_dot));
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
             }
         });
-	return rootView;
+        return rootView;
     }
 
     @Override
