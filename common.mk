d2654a1dd2b896bdea73bb6ca56e62de3f46be5d
enesykaya <enesykaya@gmail.com>
2022-05-29 14:02:08 +0300

    msm8998-common: Build GrapheneOS camera app
    * https://github.com/yaap-op5t/platform_external_Camera

--- common.mk
+++ common.mk
@@ -133,7 +133,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    Camera
 
 # Component overrides
 PRODUCT_COPY_FILES += \
